83c7344bb1193bb194ee4d2acfd0a07e48b3ee7e
Jennifer Alexander <110498167+jsalexan@users.noreply.github.com>
2022-11-23 12:09:42 -0800

    Found one naming error in the login and corrected it. Still not working

--- public/js/login.js
+++ public/js/login.js
@@ -14,7 +14,7 @@ const loginFormHandler = async (event) => {
       });
   
       if (response.ok) {
-        // If successful, redirect the browser to the profile page
+        // If successful, redirect the browser to the dashboard
         document.location.replace('/dashboard');
       } else {
         alert(response.statusText);
@@ -37,7 +37,7 @@ const loginFormHandler = async (event) => {
       });
   
       if (response.ok) {
-        document.location.replace('/profile');
+        document.location.replace('/dashboard');
       } else {
         alert(response.statusText);
       }
